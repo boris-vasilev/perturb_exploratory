3efea65b97b55350141369520c0cb520fd9eccd1
Boris Vasilev <borisvasilev395@gmail.com>
2025-09-25 18:23:36 +0100

    Remove perturbation from select

--- create_logFC_data.R
+++ create_logFC_data.R
@@ -17,7 +17,7 @@ n_cores <- 32
 DEGs <- mclapply(DEG_files, function(f) {
   fname <- basename(f)
   message("Reading: ", f)
-  fread(f) %>% select(perturbation, gene, log2FoldChange)
+  fread(f) %>% select(gene, log2FoldChange)
 }, mc.cores = n_cores, mc.preschedule = FALSE)
 
 names(DEGs) <- sub("\\.tsv$", "", basename(DEG_files))
@@ -28,7 +28,7 @@ ncols <- sapply(DEGs, ncol)
 # Get the indices (or names) of those with just 1 column
 bad_idx <- which(ncols == 1)
 
-DEGs <- DEGs[- c(bad_idx)]
+DEGs <- DEGs[-c(bad_idx)]
 
 logFC_dt <- rbindlist(DEGs, idcol = "perturbation")
 fwrite(logFC_dt, glue("/rds/project/rds-csoP2nj6Y6Y/biv22/data/pairs/{cells}/logFC_dat.csv"))
