45b4bf12b3c41dc09fcf8dd10d3a558aa5a24abc
Boris Vasilev <borisvasilev395@gmail.com>
2025-09-25 18:19:41 +0100

    Remove empty DEG files

--- create_logFC_data.R
+++ create_logFC_data.R
@@ -21,5 +21,14 @@ DEGs <- mclapply(DEG_files, function(f) {
 }, mc.cores = n_cores, mc.preschedule = FALSE)
 
 names(DEGs) <- sub("\\.tsv$", "", basename(DEG_files))
+
+# Check number of columns for each element
+ncols <- sapply(DEGs, ncol)
+
+# Get the indices (or names) of those with just 1 column
+bad_idx <- which(ncols == 1)
+
+DEGs <- DEGs[-bad_idx]
+
 logFC_dt <- rbindlist(DEGs, idcol = "perturbation")
 fwrite(logFC_dt, glue("/rds/project/rds-csoP2nj6Y6Y/biv22/data/pairs/{cells}/logFC_dat.csv"))
